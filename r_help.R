49aa946d27dc060be69cd004c00a437ad0f7c6e2
Haylie Wu <nw2455@columbia.edu>
2022-03-22 23:01:36 -0400

    coding etiquette

--- r_help.R
+++ r_help.R
@@ -15,8 +15,6 @@ boxTidwell(logodds ~ income_log + property_value_log + combined_loan_to_value_ra
 
 #install.packages("GPArotation")
 
-
-
 fa <- fa(mortgage, nfactors = 3, fm = "pa", rotate = "none", max.iter = 100)
 fa.rotate <- fa(mortgage, nfactors = 3, fm = "pa", rotate = "varimax", max.iter = 100)
 fa.rotate$loadings
@@ -24,7 +22,7 @@ fa.rotate$loadings
 factors <- fa.rotate$scores[,1:3]
 colnames(factors) <- c("Financial", "Race+Fin","Other")
 
-
+# data descrpition
 describe(factors)
 helo <- glm(action_taken ~ debt_to_income_ratio + applicant_age + loan_amount_log + income_log + property_value_log + combined_loan_to_value_ratio + loan_term + ffiec_msa_md_median_family_income + tract_to_msa_income_percentage, data=mortgage, family = binomial(link="logit"))
 
